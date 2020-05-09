0039b63efdf94f9c2bd3877400a940bdefd8322e
Mikolaj-Kr <mikolak25@gmail.com>
2020-05-09 17:41:42 +0200

    Add project

---
+++ src/test/java/com/krawczak/mapApp/mapApp/MapAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krawczak.mapApp.mapApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
